cf06a347cb36b4ee4cd2022f5cf3ba707adcb403
Levi Hopkins and Ryan Streur <levi@>
2016-06-13 10:22:13 -0700

    Add the place object with a few relevant properties. Add one object for Amsterdam

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
+//Back End
+function Place(name, country, landmarks, timeOfYear, notes, imgLink){
+  this.name = name;
+  this.country = country;
+  this.landmarks = landmarks;
+  this.timeOfYear = timeOfYear;
+  this.notes = notes;
+  this.imgLink = imgLink;
+}
+
+var amsterdam = new Place("Amsterdam","The Netherlands","Anne Frank House","Spring","tulips","img/amsterdam.jpg")
+
+//Front End
 $(document).ready(function(){
-  
 
 });
