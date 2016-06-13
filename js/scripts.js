f80b2d2ea15d57a29643bd735f5dbed7f3e80db7
Levi Hopkins and Ryan Streur <ryan@>
2016-06-13 11:46:49 -0700

    Add two loops to the UI logic which enable us to display an arbitrary number of Place instances without writing more code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 //Back End
+var globalPlaceArray = [];
 function Place(name, country, landmarks, timeOfYear, notes, imgLink){
   this.name = name;
   this.country = country;
@@ -6,20 +7,27 @@ function Place(name, country, landmarks, timeOfYear, notes, imgLink){
   this.timeOfYear = timeOfYear;
   this.notes = notes;
   this.imgLink = imgLink;
+  globalPlaceArray.push(this);
 }
 
 var amsterdam = new Place("Amsterdam","The Netherlands","Anne Frank House","Spring","tulips","img/amsterdam.jpg")
+var lasvegas = new Place("Las Vegas","United States of America","Casinos","Summer","Boss af","img/lasvegas.jpg")
 
 //Front End
 $(document).ready(function(){
-  $(".place-list").append("<li class='placename'>"+amsterdam.name+"</li>");
-  $(".placename").last().click(function(event){
+  for (var i=0; i<globalPlaceArray.length; i++){
+    $(".place-list").append("<li class='placename place"+i+"'>"+globalPlaceArray[i].name+"</li>");
+
+  };
+  i=0;
+  $(".placename").click(function(event){
+    debugger;
+    var thisClass = $(this).attr("class");
+    var j = thisClass.charAt(thisClass.length-1);
+    console.log(j);
     event.preventDefault();
-    $(".infoList").append("<li class='infoItem'>Place Name: " + amsterdam.name + "</li><li>Country: "+amsterdam.country+"</li><li>Landmarks: " + amsterdam.landmarks + "</li><li>Time of Year Visited: " + amsterdam.timeOfYear + "</li><li>Notes: " + amsterdam.notes + "</li>");
-    $(".placeImage").append("<img class='infoItem' src='"+amsterdam.imgLink+"' alt='Amsterdam'>");
-
-    $(".infoItem").click(function(){
-
-    })
+    $(".infoItem").remove();
+    $(".infoList").append("<li class='infoItem'>Place Name: " + globalPlaceArray[j].name + "</li><li class='infoItem'>Country: "+globalPlaceArray[j].country+"</li><li class='infoItem'>Landmarks: " + globalPlaceArray[j].landmarks + "</li><li class='infoItem'>Time of Year Visited: " + globalPlaceArray[j].timeOfYear + "</li><li class='infoItem'>Notes: " + globalPlaceArray[j].notes + "</li>");
+    $(".placeImage").append("<img class='infoItem' src='"+globalPlaceArray[j].imgLink+"' alt='Amsterdam'>");
   });
 });
